9ec98ff19725d97e90a0d8a1c38c1e57cf9ede81
Mikel Larreategi <mlarreategi@codesyntax.com>
2020-09-07 12:09:41 +0200

    move things around

--- setup.py
+++ setup.py
@@ -1,40 +1,44 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = "1.0"
 
-setup(name='cs.publiccontracts',
-      version=version,
-      description="Public Contracs Product",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name="cs.publiccontracts",
+    version=version,
+    description="Public Contracs Product",
+    long_description=open("README.rst").read()
+    + "\n"
+    + open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['cs'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'five.grok',
-          'plone.app.dexterity [grok, relations]',
-          'plone.namedfile [blobs]',
-          'collective.z3cform.datagridfield',
-          'collective.dexteritytextindexer'
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
+    ],
+    keywords="",
+    author="",
+    author_email="",
+    url="http://svn.plone.org/svn/collective/",
+    license="GPL",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    namespace_packages=["cs"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        "five.grok",
+        "plone.app.dexterity [grok, relations]",
+        "plone.namedfile [blobs]",
+        "collective.z3cform.datagridfield",
+        "collective.dexteritytextindexer"
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
-      """,)
+      """,
+)
